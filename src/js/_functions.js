ff2c2b3425683c37947281f6187df683565a0945
stavbers <reddevweb@gmail.com>
2022-08-05 22:52:06 +0300

    add basic files and image

---
+++ src/js/_functions.js
@@ -0,0 +1,28 @@
+// Данный файл - лишь собрание подключений готовых компонентов.
+// Рекомендуется создавать отдельный файл в папке components и подключать все там
+
+// Определение операционной системы на мобильных
+import { mobileCheck } from "./functions/mobile-check";
+console.log(mobileCheck())
+
+// Определение ширины экрана
+// import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
+// if (isDesktop()) {
+//   console.log('...')
+// }
+
+// Получение высоты шапки сайта (не забудьте вызвать функцию)
+// import { getHeaderHeight } from './functions/header-height';
+
+
+// Подключение анимаций по скроллу
+// import AOS from 'aos';
+// AOS.init();
+
+// Подключение параллакса блоков при скролле
+// import Rellax from 'rellax';
+// const rellax = new Rellax('.rellax');
+
+// Подключение плавной прокрутки к якорям
+// import SmoothScroll from 'smooth-scroll';
+// const scroll = new SmoothScroll('a[href*="#"]');
